8171e05b0aa4dd5ac5c304e772469c6c6b38affc
“poncedavid” <Da.ponce@duocuc.cl>
2024-02-23 21:21:34 -0300

    Agregando contexto y componentes en las páginas Home y Service.

--- app-dojo/src/Pages/Service/index.jsx
+++ app-dojo/src/Pages/Service/index.jsx
@@ -1,10 +1,15 @@
 // importando Layout.
 import Layout from "../../Layout";
 
+// importando componentes.
+import Formulario from "../../Components/Formulario";
+
 function Service() {
     return (
         <Layout>
         <h1>Service</h1>
+
+        <Formulario/>
         </Layout>
     );
 }
